d912f2345bf9c2ac6145ec3c702512650693849e
Ethan Lin <midgetlin2003@gmail.com>
2025-05-19 19:16:11 -0700

    Installed earlier tailwindcss dependency

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ZotSwap from "../assets/zotswap.png";
+
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <img src={ZotSwap} alt="ZotSwap logo"/>
+        </div>
+    )
+}
+
+export default Home;
